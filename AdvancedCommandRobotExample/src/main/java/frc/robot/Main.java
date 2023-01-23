1cb88ee7f30c1f2f80af032c20b95b18f081ce04
team614robotics <101016450+SeanLuaTech@users.noreply.github.com>
2023-01-23 16:09:24 -0500

    remove Comment Testing

--- AdvancedCommandRobotExample/src/main/java/frc/robot/Main.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/Main.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-// Comment Test 2
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
