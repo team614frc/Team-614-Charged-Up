b59ac4705f11498e8faec642e0bee6faac75ef1f
SeanLuaTech <101016450+SeanLuaTech@users.noreply.github.com>
2023-01-24 15:51:56 -0500

    Remove driveforwards/drive backwards import

--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/AutoChargeStationNotEngaged.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/AutoChargeStationNotEngaged.java
@@ -5,7 +5,7 @@
 package frc.robot.commands.Autonomous;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.DriveForwards;
+
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
