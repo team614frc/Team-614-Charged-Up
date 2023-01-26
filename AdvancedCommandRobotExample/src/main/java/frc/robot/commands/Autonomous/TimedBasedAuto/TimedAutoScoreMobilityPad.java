d4dcf661d64cbf42245985731152e462006d1e9d
SeanLuaTech <101016450+SeanLuaTech@users.noreply.github.com>
2023-01-26 11:06:25 -0500

    Removed Unused and Unneeded Import
    Removed frc.robot.Robot import

--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/TimedAutoScoreMobilityPad.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/TimedAutoScoreMobilityPad.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.MotorSafety;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
