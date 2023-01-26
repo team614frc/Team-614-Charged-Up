897bdba89c6436899854ad4823bcb357fbb375e2
SeanLuaTech <101016450+SeanLuaTech@users.noreply.github.com>
2023-01-26 12:16:08 -0500

    Commented out unused import

--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/TimedAutoScoreMobilityPad.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/TimedAutoScoreMobilityPad.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 //import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.commands.DriveDirection;
+//import frc.robot.commands.DriveDirection; Uncomment when you will use
 
 public class TimedAutoScoreMobilityPad extends CommandBase {
   /** Creates a new TimedAuto. */
