230007eb278d628cac43f6692a3fa32efa653f70
SeanLuaTech <101016450+SeanLuaTech@users.noreply.github.com>
2023-01-26 11:19:34 -0500

    Add Import

--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/AutoScoreMobilityPad.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/AutoScoreMobilityPad.java
@@ -6,7 +6,7 @@ package frc.robot.commands.Autonomous.TimedBasedAuto;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-//import frc.robot.Constants;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 public class AutoScoreMobilityPad extends CommandBase {
