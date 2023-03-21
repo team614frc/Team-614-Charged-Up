6baea04fee2c41ab4a5b9878d99597e95c24a038
HighSkai <Skai.ronis1@gmail.com>
2023-03-21 10:04:38 -0400

    Abstract eventmaploader

--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/TestAuto.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/Autonomous/TimedBasedAuto/TestAuto.java
@@ -2,10 +2,11 @@ package frc.robot.commands.Autonomous.TimedBasedAuto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.PathPlannerLoadEventMapCommand;
+import frc.robot.commands.getEventMap;
 
 public class TestAuto extends SequentialCommandGroup {
   public TestAuto() {
     addCommands(
-        new PathPlannerLoadEventMapCommand().withTimeout(15));
+        new PathPlannerLoadEventMapCommand("Marker1Path1", getEventMap.ScoreAutoBalanceBlue()).withTimeout(15));
   }
 }
--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/PathPlannerLoadEventMapCommand.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/PathPlannerLoadEventMapCommand.java
@@ -2,30 +2,32 @@ package frc.robot.commands;
 
 import java.util.HashMap;
 import java.util.List;
+
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.auto.RamseteAutoBuilder;
+
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
 public class PathPlannerLoadEventMapCommand extends InstantCommand {
+  private final String filename;
+  private final HashMap<String, Command> eventmap;
 
-  public PathPlannerLoadEventMapCommand() {
+  public PathPlannerLoadEventMapCommand(String fileName, HashMap<String, Command> eventMap) {
+    this.filename = fileName;
+    this.eventmap = eventMap;
   }
 
   @Override
   public void initialize() {
-    List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("ScoreGrabBalance", new PathConstraints(2, 2));
+    List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(filename, new PathConstraints(2, 2));
 
-    HashMap<String, Command> eventMap = new HashMap<>();
-    eventMap.put("intake", new PrintCommand("left"));
-    eventMap.put("right", new PrintCommand("right"));
-    eventMap.put("event", new PrintCommand("end"));
+    HashMap<String, Command> eventMap = eventmap;
 
     RamseteAutoBuilder autoBuilder = new RamseteAutoBuilder(
         RobotContainer.driveTrainSubsystem::getPose,
