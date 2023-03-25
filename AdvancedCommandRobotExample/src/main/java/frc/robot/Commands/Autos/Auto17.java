6ea27043b5a6638fd627471469f86fbd922a77a5
HighSkai <Skai.ronis1@gmail.com>
2023-03-25 09:08:41 -0400

    Final Fixes

--- AdvancedCommandRobotExample/src/main/java/frc/robot/Commands/Autos/Auto17.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/Commands/Autos/Auto17.java
@@ -4,29 +4,21 @@
 
 package frc.robot.Commands.Autos;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Commands.PathPlannerCommands.PathPlannerLoadPathCommand;
+import frc.robot.Commands.SequentialParallelCommands.ScoreHighCubeAuto;
 
-public class Auto17 extends CommandBase {
-  /** Creates a new Auto17. */
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Auto17 extends SequentialCommandGroup {
+  /** Creates a new Auto16. */
   public Auto17() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new ScoreHighCubeAuto(),
+      new PathPlannerLoadPathCommand("pathplanner/generatedJSON/RTCMM.wpilib.json", true)
+    );
   }
 }
