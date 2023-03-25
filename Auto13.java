9da18978e46d6744695d6003a64387c1eb91a9e3
Skai <91103467+HighSkai@users.noreply.github.com>
2023-03-24 23:51:52 -0400

    Add files via upload

---
+++ Auto13.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous.TimedBasedAuto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.PathPlannerLoadPathCommand;
+import frc.robot.commands.SequentialParallelCommands.ScoreHighCubeAuto;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Auto13 extends SequentialCommandGroup {
+  /** Creates a new Auto13. */
+  public Auto13() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new ScoreHighCubeAuto(),
+      new PathPlannerLoadPathCommand("pathplanner/generatedJSON/BTCMM.wpilib.json", true)
+    );
+  }
+}
---
+++ Auto7.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous.TimedBasedAuto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.PathPlannerLoadPathCommand;
+import frc.robot.commands.SequentialParallelCommands.ScoreMidCone;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Auto7 extends SequentialCommandGroup {
+  /** Creates a new Auto7. */
+  public Auto7() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+       new ScoreMidCone(),
+       new PathPlannerLoadPathCommand("pathplanner/generatedJSON/BTCMM.wpilib.json", true)
+    );
+  }
+}
